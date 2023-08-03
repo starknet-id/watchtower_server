8942eae68c7f5ba04dc84be7d680e0ef04a10d00
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-08-03 17:29:31 +0200

    Adding manual save system

---
+++ src/utils/user/db/save_db.rs
@@ -0,0 +1,39 @@
+use mongodb::{
+    bson::{doc, oid::ObjectId, Document},
+    Collection, Database,
+};
+
+pub async fn save_db(
+    db: &Database,
+    client_connection_string: String,
+    db_name: String,
+    db_id: ObjectId,
+) {
+    // Download client database and store it in the db_saves folder
+    // Name it using the db_id and the current date
+    // Then save the path in the db_saves collection
+    let current_date = chrono::Utc::now();
+    let timestamp = current_date.timestamp_millis();
+    let out = format!("db_saves/{}/{}", db_id, timestamp.clone());
+    let command = format!(
+        "mongodump --out {} --gzip --db {} --uri \"{}\"",
+        out, db_name, client_connection_string
+    );
+
+    let output = std::process::Command::new("cmd")
+        .args(&["/C", &command])
+        .output();
+
+    if output.is_err() {
+        println!("Error while saving db: {}", output.err().unwrap());
+        return;
+    }
+
+    let collection: Collection<Document> = db.collection("db_saves");
+    let document = doc! {
+        "db_id": db_id,
+        "time": timestamp,
+    };
+
+    collection.insert_one(document, None).await.unwrap();
+}
---
+++ src/utils/user/db/secure_save_db.rs
@@ -0,0 +1,51 @@
+use mongodb::{
+    bson::{doc, oid::ObjectId, Document},
+    Collection, Database,
+};
+
+use super::save_db::save_db;
+use super::update_db_datas::update_db_datas;
+
+pub struct CheckDbRes {
+    pub success: bool,
+    pub message: String,
+}
+
+pub async fn secure_save_db(
+    collection: Collection<Document>,
+    db: &Database,
+    connection_string: String,
+    db_name: String,
+    db_id: ObjectId,
+) -> CheckDbRes {
+    // Update db status
+    collection
+        .update_one(
+            doc! {"_id": db_id},
+            doc! {"$set": {"status": "connecting"}},
+            None,
+        )
+        .await
+        .unwrap();
+    let res = update_db_datas(
+        collection,
+        connection_string.to_string(),
+        db_name.to_string(),
+        db_id.to_hex(),
+    )
+    .await;
+
+    if res.client_db.is_some() {
+        save_db(
+            db,
+            connection_string.to_string(),
+            db_name.to_string(),
+            db_id,
+        )
+        .await;
+    }
+    return CheckDbRes {
+        success: res.success,
+        message: res.message,
+    };
+}
